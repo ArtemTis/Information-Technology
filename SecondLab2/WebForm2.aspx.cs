93e06a5af3055116e8eea90e9592574379e24f4d
Artem Tishenko <87804006+ArtemTis@users.noreply.github.com>
2022-03-31 22:27:50 +0400

    Add files via upload

---
+++ SecondLab2/WebForm2.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SecondLab2
+{
+    public partial class WebForm2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void InputPassword_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Submit_Click(object sender, EventArgs e)
+        {
+            Session["Login"] = Login.Text;
+            Session["Password"] = Password.Text;
+
+            Response.Redirect("WebForm3.aspx");
+        }
+    }
+}
---
+++ SecondLab2/WebForm3.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SecondLab2
+{
+    public partial class WebForm3 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            LabelLogin.Text = Session["Login"].ToString();
+            LabelPassword.Text = Session["Password"].ToString();
+        }
+    }
+}
